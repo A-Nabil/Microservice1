2a48bb3dce41aeff17829621cbb27dc74010eb0b
Ahmed <ahmed.nabil@storytel.com>
2022-05-15 15:58:34 +0200

    service1 end-point

---
+++ Microservice1/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Microservice1.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Data = new[]
+        {
+            "service1", "service1", "service1", "service1", "service1"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TestController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("service1")]
+        public ActionResult Get()
+        {
+            return Ok(Data);
+        }
+    }
+}
